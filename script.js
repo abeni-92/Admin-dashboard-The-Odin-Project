3a142b7bf2a9dd9c4715b95a147cd958cfc999d1
abenezertesfaye <abenezertesfaye92@gmail.com>
2023-07-28 11:26:55 -0700

    media queries added to make it responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+const bar = document.getElementById('show');
+const sideBar = document.querySelector('.sidebar');
+const main = document.querySelector('main');
+
+bar.addEventListener('click', () => {
+	
+})
